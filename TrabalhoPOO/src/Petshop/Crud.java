b2d3fd6581c5e1b5bab62c95aed056b9d996a963
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2025-07-17 22:31:02 -0300

    .

---
+++ TrabalhoPOO/src/Petshop/Crud.java
@@ -0,0 +1,9 @@
+package Petshop;
+
+public interface Crud {
+    public void cadastrar();
+    public void consultar();
+    public void alterar();
+    public void remover();
+    public void relatorio();
+}
