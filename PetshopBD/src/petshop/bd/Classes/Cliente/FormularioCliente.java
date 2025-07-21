aba84fe649a358ec7c800771307dee32ca58ed47
Daniella Yamasaki <danimidoriyama@gmail.com>
2025-07-21 14:11:04 -0300

    botao

--- PetshopBD/src/petshop/bd/Classes/Cliente/FormularioCliente.java
+++ PetshopBD/src/petshop/bd/Classes/Cliente/FormularioCliente.java
@@ -43,7 +43,7 @@ public class FormularioCliente extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jPanel5 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
-        jButton3 = new javax.swing.JButton();
+        botaoVoltar = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         campoNome = new javax.swing.JFormattedTextField();
         campoEmail = new javax.swing.JFormattedTextField();
@@ -56,7 +56,7 @@ public class FormularioCliente extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        botaoCadastrar = new javax.swing.JButton();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -137,14 +137,14 @@ public class FormularioCliente extends javax.swing.JFrame {
         jPanel3.setForeground(new java.awt.Color(0, 51, 51));
         jPanel3.setPreferredSize(new java.awt.Dimension(0, 2));
 
-        jButton3.setBackground(new java.awt.Color(102, 102, 102));
-        jButton3.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3.setText("Voltar");
-        jButton3.setIconTextGap(3);
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        botaoVoltar.setBackground(new java.awt.Color(102, 102, 102));
+        botaoVoltar.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+        botaoVoltar.setForeground(new java.awt.Color(255, 255, 255));
+        botaoVoltar.setText("Voltar");
+        botaoVoltar.setIconTextGap(3);
+        botaoVoltar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                botaoVoltarActionPerformed(evt);
             }
         });
 
@@ -266,19 +266,19 @@ public class FormularioCliente extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(51, 51, 51));
         jLabel2.setText("Cadastro Cliente");
 
-        jButton2.setBackground(new java.awt.Color(0, 51, 51));
-        jButton2.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(242, 242, 242));
-        jButton2.setText("Cadastrar");
-        jButton2.setIconTextGap(3);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        botaoCadastrar.setBackground(new java.awt.Color(0, 51, 51));
+        botaoCadastrar.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+        botaoCadastrar.setForeground(new java.awt.Color(242, 242, 242));
+        botaoCadastrar.setText("Cadastrar");
+        botaoCadastrar.setIconTextGap(3);
+        botaoCadastrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                botaoCadastrarActionPerformed(evt);
             }
         });
-        jButton2.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+        botaoCadastrar.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
             public void propertyChange(java.beans.PropertyChangeEvent evt) {
-                jButton2PropertyChange(evt);
+                botaoCadastrarPropertyChange(evt);
             }
         });
 
@@ -313,7 +313,7 @@ public class FormularioCliente extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(botaoCadastrar, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(115, 115, 115))
         );
         jPanel2Layout.setVerticalGroup(
@@ -342,7 +342,7 @@ public class FormularioCliente extends javax.swing.JFrame {
                     .addComponent(campoRG, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(campoCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(botaoCadastrar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(27, Short.MAX_VALUE))
         );
 
@@ -356,7 +356,7 @@ public class FormularioCliente extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton3)
+                .addComponent(botaoVoltar)
                 .addGap(22, 22, 22))
         );
         jPanel3Layout.setVerticalGroup(
@@ -365,7 +365,7 @@ public class FormularioCliente extends javax.swing.JFrame {
                 .addGap(47, 47, 47)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
-                .addComponent(jButton3)
+                .addComponent(botaoVoltar)
                 .addGap(20, 20, 20))
         );
 
@@ -387,17 +387,17 @@ public class FormularioCliente extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void botaoCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoCadastrarActionPerformed
+        //OIIIIIIIIIIIIIIIII
+    }//GEN-LAST:event_botaoCadastrarActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void botaoVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoVoltarActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_botaoVoltarActionPerformed
 
-    private void jButton2PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jButton2PropertyChange
+    private void botaoCadastrarPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_botaoCadastrarPropertyChange
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2PropertyChange
+    }//GEN-LAST:event_botaoCadastrarPropertyChange
 
     private void campoNomeFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_campoNomeFocusGained
         if(campoNome.getText().equals("Digite o Nome")){
@@ -528,14 +528,14 @@ public class FormularioCliente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botaoCadastrar;
+    private javax.swing.JButton botaoVoltar;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JFormattedTextField campoCPF;
     private javax.swing.JFormattedTextField campoEmail;
     private javax.swing.JFormattedTextField campoNome;
     private javax.swing.JFormattedTextField campoRG;
     private javax.swing.JFormattedTextField campoTelefone;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
